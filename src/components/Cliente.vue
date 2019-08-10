283e39ac69678a24408077f20387b2ac75f5d645
JaviFS97 <javifs97@gmail.com>
2019-08-10 18:27:11 +0200

    Cap. 10 - Propiedad Computed La propiedad computed sirve para no añadir expresiones complejas js al render declarativo  del codigo html. Es decir, se crean funciones js que contengan dichas expresiones para solo tener que llamar  desde el render declarativo al nombre de la funcion alojada en la propiedad computed. En este caso, hemos creado dos funciones: 	1. nombreCompleto => devuelve la concatenacion del nombre y del apellido. De esta manera no tenemos que realizar la concatenacion en el render declarativo en el html. 	2. edad => creamos una propiedad que contiene la fecha de nacimiento, a partir de esta propiedad calculamos cual es la edad.

--- src/components/Cliente.vue
+++ src/components/Cliente.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="seccion-cliente">
     <div>
-      <!-- Expresion js -->
+      <!-- Expresion js 
       <span> {{ nombres + " " + apellidos }}</span>
+      -->
+      <span> {{ nombreCompleto }}</span>
     </div>
     <div>
       <span>Edad: {{ edad }}</span>
@@ -20,7 +22,29 @@
       return {
         nombres: 'Bob',
         apellidos: 'Willson',
-        edad:25
+        fechaNacimiento : new Date(1987, 7, 10)
+      }
+    },
+
+    /**
+     * La propiedad computed sirve para no añadir expresiones complejas js al render declarativo
+     * del codigo html.
+     * Es decir, se crean funciones js que contengan dichas expresiones para solo tener que llamar
+     * desde el render declarativo al nombre de la funcion alojada en la propiedad computed.
+     */
+    computed: {
+      // nombreCompleto: function () { ==  nombreCompleto() {
+      nombreCompleto() {
+        return this.nombres + " " + this.apellidos
+      },
+      
+      /**
+       * En la base de datos no se guarda una edad, se guarda una fecha de nacimiento y de ahi
+       * se calcula la edad.
+       */
+      edad() {
+        let fechaActual = new Date()
+        return Math.floor((fechaActual - this.fechaNacimiento) / ( 1000 * 365.25 * 24 * 60 * 60))
       }
     }
 
