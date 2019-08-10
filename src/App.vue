94cb07142728ec2a92e756b4cc0b38b3f1f146ca
JaviFS97 <javifs97@gmail.com>
2019-08-10 16:57:35 +0200

    Cap. 12 - Instancia Vue Elimino las vistas predefinidas de la aplicacion y trabajo sobre la vista app, dotandola de un estilo css.

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,27 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div id="app" class="principal">
+    <span>App</span>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+<script>
+  export default {
+    
+  }
+</script>
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+
+<style >
+
+  .principal{
+    background-color: rgb(182, 18, 18);
+    padding: 10px;
+    color: white;
+    border-radius: 5px;
+    width: 400px;
+    text-align: center;
+    font-size: 2rem;
+  }
+  
 </style>
