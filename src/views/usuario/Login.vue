a36ec31788226edea81ae1b9c92e77750fa31748
JaviFS97 <javifs97@gmail.com>
2019-08-26 18:10:42 +0200

    Cap. 67 - Validacion Login: email. Utilizando la libreria Vuelidate, validamos el campo email del componente Login. Añadiendo las validaciones: requerido rellenar el campo, que el email introducido sea del formato email.

--- src/views/usuario/Login.vue
+++ src/views/usuario/Login.vue
@@ -9,7 +9,7 @@
                         </v-toolbar-title>
                     </v-toolbar>
                     <v-card-text>
-                        <v-text-field label="Email"></v-text-field>
+                        <v-text-field label="Email" v-model="email" :error-messages="this.erroresEmail" @blur="$v.email.$touch()"></v-text-field>
                         <v-text-field label="Password" type="password"></v-text-field>
                     </v-card-text>
                     <v-card-text>     
@@ -24,3 +24,35 @@
         </v-flex>
     </v-layout>
 </template>
+
+<script>
+
+    // Importamos las funciones que vamos a usar para la validacion de la variable 'email'.
+    import {required, email} from 'vuelidate/lib/validators'
+
+    export default {
+        data(){
+            return {
+                email: ''
+            }
+        },
+        // Propiedad de Vuelidate
+        validations: {
+            // Validamos la variable con nombre 'email'.
+            email: {
+                required,   // Se debe introducir un valor.
+                email       // El valor debe tener formato de email.
+            }
+        },
+        computed: {
+            erroresEmail() {
+                let errores = []
+                if (!this.$v.email.$dirty){ return errores}
+                if (!this.$v.email.required){ errores.push("Ingresa tu email.")}
+                if (!this.$v.email.email){ errores.push("Ingresa un email valido.")}
+                return errores
+        }
+        }
+
+    }
+</script>
