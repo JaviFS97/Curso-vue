926ee2e90512ce57c0a3172e297e7838d54ff0e4
JaviFS97 <javifs97@gmail.com>
2019-08-26 17:03:20 +0200

    Seccion Vuelidate. Instalacion y instanciacion de vuelidate para su uso en la app.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import Vuelidate from 'vuelidate'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.config.productionTip = false
+Vue.use(Vuelidate)
 
 new Vue({
   vuetify,
