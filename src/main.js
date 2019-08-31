3b5fb060d73c03e042a2e3a4fdc0ff743f78cd34
JaviFS97 <javifs97@gmail.com>
2019-08-31 17:33:22 +0200

    Seccion Vue Router.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import Vuelidate from 'vuelidate'
 import store from './store'
+import router from './router'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
@@ -12,5 +13,6 @@ Vue.use(Vuelidate)
 new Vue({
   vuetify,
   store,
+  router,
   render: h => h(App)
 }).$mount('#app')
